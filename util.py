306344122aa11d505a448fcef037cdfe4bef3dce
alexsiracusa <alexander.siracusa@gmail.com>
2025-12-04 15:49:08 -0500

    basic ppca and noise adding code

---
+++ util.py
@@ -0,0 +1,74 @@
+
+
+import numpy as np
+import torch
+import math
+import matplotlib.pyplot as plt
+
+
+def ppca(X, n_components=16):
+    n, m = X.shape
+    d = n_components
+
+    mean = X.mean(axis=0)                                   # (m)
+
+    S = (1 / n) * np.dot((X - mean).T, (X - mean))          # (m ,m)
+
+    eigenvalues, eigenvectors = np.linalg.eigh(S)
+    idx = np.argsort(-np.abs(eigenvalues))
+    eigenvalues = eigenvalues[idx]                          # (m)
+    eigenvectors = eigenvectors[:, idx].T                   # (m, m)     - rows are the eigenvectors
+
+    var = 1 / (m - d) * np.sum(eigenvalues[d:m])            # (1)
+    U_q = eigenvectors[:d].T                                # (m, d)
+    L = np.diag(eigenvalues[:d])                            # (d, d)
+
+    W = np.dot(U_q, np.sqrt(L - var * np.eye(d)))           # (m, d)
+    M = np.dot(W.T, W) + var * np.eye(d)                    # (d, d)
+
+    # Z = np.linalg.solve(M, np.eye(d)) @ W.T @ (X - mean).T  # (d, n)
+    # X_hat = (W @ Z).T + mean                                # (n, m)
+
+    return W, mean, M
+
+
+def generate_dataset(path='./data/faces_vae.npy', noise=0.0):
+    X = torch.tensor(np.load(path)).flatten(start_dim=1) / 255  # (n_samples, x_dim)
+
+    return (1 - noise) * X + noise * torch.randn_like(X)
+
+
+if __name__ == '__main__':
+    X = generate_dataset('./data/faces_vae.npy', noise=0.3).numpy()
+    d = 8
+
+    W, mean, M = ppca(X, n_components=d)
+
+    print(f'W: {W.shape}')
+    print(f'μ: {mean.shape}')
+    print(f'M: {M.shape}')
+
+    # Generate images to ensure PPCA is working
+    n_images = 25
+
+    Z_generated = np.random.standard_normal(size=(d, n_images))
+    X_generated = (W @ Z_generated).T + mean
+    images = X_generated.reshape((n_images, 24, 24))
+
+    size = int(math.sqrt(n_images))
+    fig, axes = plt.subplots(size, size, figsize=(size, size), facecolor="black")
+
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap='gray', interpolation='nearest', vmin=0, vmax=1)
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+    fig.clf()
+    plt.clf()
+
+
+
+
+
+
